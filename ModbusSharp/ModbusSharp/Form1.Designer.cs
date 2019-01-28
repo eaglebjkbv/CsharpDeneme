c403a114202256c86d99ca03cfd01a6d80c3569e
unknown <bbvardal@gmail.com>
2019-01-28 14:12:21 +0300

    Modbus C Sharp Program Added

---
+++ ModbusSharp/ModbusSharp/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace ModbusSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBaudrate = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.btnBaglantiKes = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbPortAdi = new System.Windows.Forms.ComboBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtKomut = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtParametre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCevap = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.labelKomutDizesi = new System.Windows.Forms.Label();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port Numarası";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Baudrate";
+            // 
+            // cmbBaudrate
+            // 
+            this.cmbBaudrate.FormattingEnabled = true;
+            this.cmbBaudrate.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cmbBaudrate.Location = new System.Drawing.Point(118, 41);
+            this.cmbBaudrate.Name = "cmbBaudrate";
+            this.cmbBaudrate.Size = new System.Drawing.Size(83, 21);
+            this.cmbBaudrate.TabIndex = 2;
+            this.cmbBaudrate.Text = "9600";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Veri Tipi";
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Location = new System.Drawing.Point(5, 19);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(75, 26);
+            this.btnBaglan.TabIndex = 3;
+            this.btnBaglan.Text = "Bağlan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnBaglantiKes
+            // 
+            this.btnBaglantiKes.Location = new System.Drawing.Point(86, 19);
+            this.btnBaglantiKes.Name = "btnBaglantiKes";
+            this.btnBaglantiKes.Size = new System.Drawing.Size(83, 26);
+            this.btnBaglantiKes.TabIndex = 3;
+            this.btnBaglantiKes.Text = "Bağlantıyı Kes";
+            this.btnBaglantiKes.UseVisualStyleBackColor = true;
+            this.btnBaglantiKes.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.cmbPortAdi);
+            this.groupBox1.Controls.Add(this.cmbBaudrate);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(381, 84);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bağlantı";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(210, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "8,O,1";
+            // 
+            // cmbPortAdi
+            // 
+            this.cmbPortAdi.FormattingEnabled = true;
+            this.cmbPortAdi.Location = new System.Drawing.Point(19, 41);
+            this.cmbPortAdi.Name = "cmbPortAdi";
+            this.cmbPortAdi.Size = new System.Drawing.Size(80, 21);
+            this.cmbPortAdi.TabIndex = 2;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(15, 43);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(38, 20);
+            this.txtAdres.TabIndex = 1;
+            this.txtAdres.Text = "01";
+            // 
+            // txtKomut
+            // 
+            this.txtKomut.Location = new System.Drawing.Point(67, 43);
+            this.txtKomut.Name = "txtKomut";
+            this.txtKomut.Size = new System.Drawing.Size(45, 20);
+            this.txtKomut.TabIndex = 1;
+            this.txtKomut.Text = "06";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(315, 43);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(59, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // txtParametre
+            // 
+            this.txtParametre.Location = new System.Drawing.Point(118, 43);
+            this.txtParametre.Name = "txtParametre";
+            this.txtParametre.Size = new System.Drawing.Size(137, 20);
+            this.txtParametre.TabIndex = 1;
+            this.txtParametre.Text = "20010DAC";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Komut ?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(115, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Parametre";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(312, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "CRC";
+            this.label7.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtCevap
+            // 
+            this.txtCevap.Location = new System.Drawing.Point(6, 19);
+            this.txtCevap.Multiline = true;
+            this.txtCevap.Name = "txtCevap";
+            this.txtCevap.Size = new System.Drawing.Size(368, 127);
+            this.txtCevap.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.labelKomutDizesi);
+            this.groupBox2.Controls.Add(this.btnGonder);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.txtParametre);
+            this.groupBox2.Controls.Add(this.txtAdres);
+            this.groupBox2.Controls.Add(this.txtKomut);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 196);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 143);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Modbus Komut";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(242, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "500ms Aralıkla Gönder";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // labelKomutDizesi
+            // 
+            this.labelKomutDizesi.AutoSize = true;
+            this.labelKomutDizesi.Location = new System.Drawing.Point(12, 87);
+            this.labelKomutDizesi.Name = "labelKomutDizesi";
+            this.labelKomutDizesi.Size = new System.Drawing.Size(77, 13);
+            this.labelKomutDizesi.TabIndex = 3;
+            this.labelKomutDizesi.Text = "Komut Dizesi : ";
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(242, 108);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(132, 29);
+            this.btnGonder.TabIndex = 2;
+            this.btnGonder.Text = "Gönder ";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Adres";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.Parity = System.IO.Ports.Parity.Odd;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtCevap);
+            this.groupBox3.Location = new System.Drawing.Point(12, 345);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(381, 156);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Cevap";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.pictureBox1);
+            this.groupBox4.Controls.Add(this.btnBaglan);
+            this.groupBox4.Controls.Add(this.btnBaglantiKes);
+            this.groupBox4.Location = new System.Drawing.Point(13, 102);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(380, 53);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Bağlantı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Offline";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Image = global::ModbusSharp.Properties.Resources.kirmizilamba;
+            this.pictureBox1.Location = new System.Drawing.Point(337, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 559);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Modbus RTU Haberleşme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBaudrate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.Button btnBaglantiKes;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtKomut;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtParametre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCevap;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbPortAdi;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Label labelKomutDizesi;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ ModbusSharp/ModbusSharp/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ModbusSharp
+{
+    public partial class Form1 : Form
+    {
+        byte[] inDataa = new byte[100];
+        int iUzunluka;
+        bool hata=false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnBaglantiKes.Enabled = false;
+            groupBox2.Enabled = false;
+            if (SerialPort.GetPortNames().Length != 0)
+            {
+                String[] portlar = SerialPort.GetPortNames();
+                cmbPortAdi.Text = portlar[0];
+                foreach (string port in portlar)
+                {
+                    cmbPortAdi.Items.Add(port);
+                }
+            }
+            ToolTip yourToolTip = new ToolTip();
+            //The below are optional, of course,
+
+            yourToolTip.ToolTipIcon = ToolTipIcon.Info;
+            yourToolTip.ToolTipTitle = "Komutlar";
+            yourToolTip.IsBalloon = true;
+            yourToolTip.ShowAlways = true;
+
+            yourToolTip.SetToolTip(label5, "01- Tek Bobin Durumu Oku \n" +
+                "02- Giriş Durumu Oku\n03- Tutucu Registerleri Oku \n" +
+                "04- Giriş Registerleri Oku \n" +
+                "05- Sadece Bir bobin durumu değiştir \n" +
+                "06- Sadece Bir Register durumunu değiştir \n" +
+                "0F- Birden fazla Bobin içeriği değiştir \n" +
+                "10- Birden fazla Registere Değer atamak ");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.PortName = cmbPortAdi.Text;
+                    serialPort1.BaudRate = Convert.ToInt16(cmbBaudrate.Text);
+                    serialPort1.Open();
+                    pictureBox1.Image = Properties.Resources.yesillamba;
+                    groupBox1.Enabled = false;
+                    btnBaglan.Enabled = false;
+                    btnBaglantiKes.Enabled = true;
+                    groupBox2.Enabled = true;
+                    label4.Text = "Online";
+
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Hata :" + exception);
+
+                }
+
+
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    pictureBox1.Image = Properties.Resources.kirmizilamba;
+                    groupBox1.Enabled = true;
+                    btnBaglan.Enabled = true;
+                    btnBaglantiKes.Enabled = false;
+                    groupBox2.Enabled = false;
+                    label4.Text = "Offline";
+
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Hata :" + exception);
+
+                }
+
+            }
+        }
+        public void veriHazirla()
+        {
+            int iUzunluk;
+            String komut = txtAdres.Text + txtKomut.Text + txtParametre.Text;
+            byte[] inData = new byte[100];
+            int j, jj, ii;
+            int reg_crc;
+
+            if (komut.Length == 0)
+            {
+                MessageBox.Show(this, "Komut Girilmedi \n - Lütfen geçerli bir komut dizesi Giriniz...", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtAdres.Focus();
+                hata = true;
+                return;
+            }
+
+            iUzunluk = komut.Length;
+           
+                if (iUzunluk % 2 != 0)
+                {
+                    MessageBox.Show(this, "Hatalı Komut Dizesi \n- Lütfen komutununuzun dizenizin doğruluğunu kontrol ediniz...", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtAdres.Focus();
+                    hata = true;
+                return;
+                
+                }
+
+
+                // CRC Hesaplama
+                iUzunluk = iUzunluk / 2;
+                for (ii = 0; ii < iUzunluk; ii++)
+                {
+                    inData[ii] = (byte)Convert.ToInt32(komut.Substring(ii * 2, 2), 16);
+
+                }
+                ii = 0;
+                jj = iUzunluk;
+                reg_crc = 0XFFFF;
+                do
+                {
+                    reg_crc = reg_crc ^ inData[ii];
+                    ii++;
+                    for (j = 0; j <= 7; j++)
+                    {
+                        if ((reg_crc & 0X1) == 1)
+                        {
+                            reg_crc = (reg_crc >> 1) ^ 0XA001;
+                        }
+                        else
+                        {
+                            reg_crc = (reg_crc >> 1);
+                        }
+
+                    }
+                    jj--;
+
+
+                } while (jj != 0);
+                inData[iUzunluk] = (byte)(reg_crc & 0XFF);
+                inData[iUzunluk + 1] = (byte)((reg_crc >> 8) & 0XFF);
+                textBox4.Text = (Convert.ToString(inData[iUzunluk], 16) + Convert.ToString(inData[iUzunluk + 1], 16)).ToUpper();
+                labelKomutDizesi.Text = "Komut Dizesi : " + komut + textBox4.Text;
+            inDataa = inData;
+            iUzunluka = iUzunluk;
+            hata = false;
+
+            }
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            veriHazirla();
+            if (!hata)
+            {
+                serialPort1.Write(inDataa, 0, iUzunluka + 2);
+                txtCevap.AppendText("*");
+            }
+    }
+
+       
+        public delegate void veriGoster(String veri);
+
+        public void texteYaz(String veri)
+        {
+            txtCevap.Text += veri;
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int rSayi,ic;
+            byte gelenVeri;
+            string gelenVeriString=null;
+            
+            veriGoster ekle=new veriGoster(texteYaz);
+            rSayi = serialPort1.BytesToRead;
+
+            for (ic = 1; ic <= rSayi; ic++)
+            {
+                gelenVeri=(byte) serialPort1.ReadByte();
+                gelenVeriString = gelenVeri.ToString("X2");
+                txtCevap.Invoke(ekle,","+gelenVeriString);
+            }
+            
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                veriHazirla();
+                timer1.Enabled = true;
+            }else
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                if (!hata)
+                {
+                    serialPort1.Write(inDataa, 0, iUzunluka + 2);
+                    txtCevap.AppendText("*");
+                }
+            }
+        }
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
+
