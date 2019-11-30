4904d52521a7fe7c8adfe79cab5c6f6891820f62
unknown <bbvardal@gmail.com>
2019-11-30 10:10:23 +0300

    TCP client and server example added

---
+++ TCPSocketClient/TCPSocketClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPSocketClient
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint=new IPEndPoint(IPAddress.Parse("127.0.0.1"),1234);
+            sck.Connect(endPoint);
+
+            Console.WriteLine("Mesaj Girin : ");
+            string msg = Console.ReadLine();
+            byte[] msgTamponu = Encoding.Default.GetBytes(msg);
+            sck.Send(msgTamponu, 0, msgTamponu.Length, 0);
+
+            byte[] tampon=new byte[255];
+            int rec = sck.Receive(tampon, 0, tampon.Length, 0);
+
+            Array.Resize(ref tampon,rec);
+            Console.WriteLine("Gelen :{0}",Encoding.Default.GetString(tampon));
+
+            Console.Read();
+        }
+    }
+}
---
+++ TCPSocketServer/TCPSocketServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPSocketServer
+{
+    class Program
+    {
+        static byte[] buffer { get; set; }
+        static Socket sck;
+
+        static void Main(string[] args)
+        {
+            sck=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(IPAddress.Any,1234));
+            sck.Listen(100);
+            
+
+            Socket kabulEdildi = sck.Accept();
+
+            buffer=new byte[kabulEdildi.SendBufferSize];
+            int byteOkunan = kabulEdildi.Receive(buffer);
+
+            byte[] duzenlenmis = new byte[byteOkunan];
+            for (int i = 0; i < byteOkunan; i++)
+            {
+                duzenlenmis[i] = buffer[i];
+            }
+
+            string strVeri = Encoding.ASCII.GetString(duzenlenmis);
+            Console.WriteLine(strVeri+"\r\n");
+            Console.Read();
+            kabulEdildi.Close();
+        }
+    }
+}
